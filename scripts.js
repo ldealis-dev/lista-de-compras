5d3a73f2d3966408c768f9917b4be6441e11bc32
ldealis-automations <dealis.luiz@gmail.com>
2024-07-25 22:07:39 -0300

    feat: add new item & remove item

---
+++ scripts.js
@@ -0,0 +1,38 @@
+
+const form = document.getElementById('form-add-item');
+const newItemInput = document.getElementById('new-item');
+const itemListElement = document.getElementById('item-list');
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    if (!newItemInput.value) {
+        return;
+    }
+    itemListElement.append(addItemToList(newItemInput.value));
+    newItemInput.value = '';
+}
+
+/**
+ * create a li element from a item name
+ * @param {string} itemName 
+ * @returns li element
+ */
+function addItemToList(itemName) {
+    const spanItemName = document.createElement('span');
+    spanItemName.textContent = itemName;
+    const imgTrashBin = document.createElement('img');
+    imgTrashBin.setAttribute('src', './assets/icons/trash-bin.png');
+    imgTrashBin.addEventListener('click', () => removeItemFromList(event.target));
+    const li = document.createElement('li');
+    li.append(spanItemName);
+    li.append(imgTrashBin);
+    return li;
+}
+
+/**
+ * remove the item from parent element
+ * @param {element} event 
+ */
+function removeItemFromList(event) {
+    event.parentNode.remove();
+}
